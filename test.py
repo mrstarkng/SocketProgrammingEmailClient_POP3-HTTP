b1fffe7c65e205324e96be25a0b85c5365ca5a0e
Nguyễn Lê Hùng <115090825+mrstarkng@users.noreply.github.com>
2024-11-04 09:30:20 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 file_path = input()
 print(file_path)
-print("C:\\Users\\ASUS ZenBook\\OneDrive\\Máy tính\\AVL.cpp")
+print("path?")
 with open(file_path, 'r') as file :
     data = file.read()
 print(data)
